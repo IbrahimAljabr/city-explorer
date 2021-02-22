54e9f199453e797bf278139c6fe7f17cf679d3d4
Ibrahim aljabr <Ibrahim_i95@hotmail.com>
2021-02-22 14:56:59 +0200

    done

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # city-explorer
+
+**Author**: Ibrahim al-jabr
+**Version**: 1.0.0 
+
+## Overview
+
+We need a faster way to find places and to daily uses .
+
+## Getting Started
+<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+- create repo
+- clone the repo
+- create the tree
+- install express 
+- connect the app, .env
+- function for handle Weather 
+- make a counstrucor and fill it with obj
+- res send the objects
+
+## Architecture
+<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+- js
+- node.js
+- express
+- cors
+
+
+## Change Log
+ <!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an examples: -->
+
+Number and name of feature:lab 6
+
+Estimate of time needed to complete: 2 hours
+
+Start time: 3
+
+Finish time: 5:30
+
+Actual time needed to complete: 2:30
+
+<!-- 01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource.
+
+## Credits and Collaborations
+ Give credit (and a link) to other people or resources that helped you build this application.  -->
+
+- Hanaa Maratouq 
+- ahmad almohammad
+- mohamad hadi
+- Bara'ah Hawary 
--- server.js
+++ server.js
@@ -11,71 +11,97 @@ require('dotenv').config();
 
 const PORT = process.env.PORT;
 
-app.get('/location',handleLocatioin);
-app.get('/weather',handleWeather);
+app.get('/location', handleLocatioin);
+app.get('/weather', handleWeather);
 
-function handleLocatioin(req,res) {
+function handleLocatioin(req, res) {
     console.log(req.query);
     let searchQuery = req.query.city;
-    let newObject =getData(searchQuery);
+    let newObject = getData(searchQuery);
     res.status(200).send(newObject);
-    
+
 }
-function handleWeather(req,res) {
+function handleWeather(req, res) {
     console.log(req.query);
     let searchQuery = req.query;
-    let newObject =getWeatherData(searchQuery);
+    let newObject = getWeatherData(searchQuery,res);
     res.status(200).send(newObject);
-    
+
 }
 function getData(searchQ) {
 
-    let locationData= require('./data/location.json');
-    let longitude=locationData[0].lon;
-    let latitude=locationData[0].lat;
-    let displayName=locationData[0].display_name;
-    let responseObject = new Citylocation(searchQ,displayName,latitude,longitude);
+    try {
 
-    return responseObject;
-}
-function getWeatherData(searchQW) {
+        let locationData = require('./data/location.json');
+        let longitude = locationData[0].lon;
+        let latitude = locationData[0].lat;
+        let displayName = locationData[0].display_name;
+        let responseObject = new Citylocation(searchQ, displayName, latitude, longitude);
 
-    let newArrWeather=[];
-    let weather= require('./data/weather.json');
-    console.log(weather);
+        return responseObject;
 
-    for (let index = 0; index < weather.data.length; index++) {
-        
-        let forecast=weather.data[index].weather.description;
-        let time=weather.data[index].datetime;
-        
-        const event = new Date(time); 
-        const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
-        let newTime =event.toLocaleDateString('en-US', options);
-
-        newArrWeather.push( new Cityweather(forecast,newTime));
-        
+    } catch (error) {
+        res.status(500).send('Sorry, something want wrong ...' + error);
     }
-    
-    return newArrWeather;
+
+}
+function getWeatherData(searchQW,res) {
+
+    try {
+
+        let newArrWeather = [];
+        let weather = require('./data/weather.json');
+        console.log(weather);
+
+        // let forecast = weather.data.map((val, idx) => val.weather.description);
+        // let time = weather.data.map((val,idx)=>val.datetime);
+        // Cityweather.map((val,idx)=>)
+        
+        weather.data.map(element => {
+
+            let forecast = element.weather.description;
+            let time = element.datetime;
+
+            const event = new Date(time);
+            const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+            let newTime = event.toLocaleDateString('en-US', options);
+
+            newArrWeather.push(new Cityweather(forecast, newTime));
+        });
+
+        // for (let index = 0; index < weather.data.length; index++) {
+            // let forecast = weather.data[index].weather.description;
+            // let time = weather.data[index].datetime;
+            // const event = new Date(time);
+            // const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+            // let newTime = event.toLocaleDateString('en-US', options);
+            // newArrWeather.push(new Cityweather(forecast, newTime));
+
+        // }
+        return newArrWeather;
+
+    } catch (error) {
+        res.status(500).send('Sorry, something want wrong ...' + error);
+    }
+
 }
 
 
-function Citylocation(searchQ, diplayName, lat ,lon) {
-    this.search_query=searchQ;
-    this.formatted_query=diplayName;
-    this.latitude=lat;
-    this.longitude=lon;
-    
+function Citylocation(searchQ, diplayName, lat, lon) {
+    this.search_query = searchQ;
+    this.formatted_query = diplayName;
+    this.latitude = lat;
+    this.longitude = lon;
+
 }
 
-function Cityweather(weather,time) {
-    this.forecast=weather;
-    this.time=time;
+function Cityweather(weather, time) {
+    this.forecast = weather;
+    this.time = time;
 }
 
 app.listen(PORT, () => {
-    console.log('the app is here at',PORT);
+    console.log('the app is here at', PORT);
 });
 
 
